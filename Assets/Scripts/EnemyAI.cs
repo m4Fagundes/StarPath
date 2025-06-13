ad7dfe27b184568f3f16aa9246db12fe52e87b13
Matheus Fagundes <124789617+m4Fagundes@users.noreply.github.com>
2025-06-13 07:13:24 -0300

    Revert "Merge pull request #3 from m4Fagundes/BranchJoao"
    This reverts commit 555b4a4bb169b0c06f23265af0b5f652d232119e, reversing
    changes made to 80ab82ca81bd3e99e69c3d960293ed0680007b13.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,7 +6,7 @@ public class EnemyAI : MonoBehaviour
 {
     public PlanetNode currentPlanet;
     private PlanetNode targetPlanet;
-    public float speed = 6f;
+    public float speed = 100f;
 
     private PlanetNode nextNode;
 
